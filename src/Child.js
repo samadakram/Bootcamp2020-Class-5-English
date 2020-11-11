762f14b7f97301cdc43119b3ef6f62e5c5602b50
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-11 18:51:53 +0500

    Update Child.js
    bug fixed and removed unused extra code

--- src/Child.js
+++ src/Child.js
@@ -8,7 +8,7 @@ const Child = () => {
             <h1>Child Component</h1>
             <h2>CHild counter number is : {value[0]} </h2>
             <button onClick={()=> value[1](++value[0])} >Update Value</button>
-            <button onClick={()=> value[1](0,value[0])} >Reset</button>
+            <button onClick={()=> value[1](0)} >Reset</button>
         </div>
     );
 }
