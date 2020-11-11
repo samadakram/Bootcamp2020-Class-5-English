f567434f32f796f8b8021c1898dca7c073284894
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-11 19:19:54 +0500

    Update Child2.js
    imported numberReducer in child2.js and set value of counter

--- src/Child2.js
+++ src/Child2.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import numberReducer from './NumberReducer';
 
 const Child2 = () => {
+
+    let [state, dispatch] = useReducer(numberReducer, 52)
     return(
         <div>
             <h1>Child 2 Component</h1>
+            <h2>Child 2 counter : {state} </h2>
         </div>
     );
 }
